8349ead95c9745ff9c33fdf704b7ab8abcac94a4
Milan Nirola <acmilan2469@gmail.com>
2021-10-20 09:56:26 +0545

    working on saving employee mvc

--- src/main/java/mn/milan/controller/EmployeeController.java
+++ src/main/java/mn/milan/controller/EmployeeController.java
@@ -1,10 +1,11 @@
 package mn.milan.controller;
 
+import mn.milan.model.Employee;
 import mn.milan.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/employee")
@@ -16,5 +17,19 @@ public class EmployeeController {
     @GetMapping("/")
     public String home(){
         return "home";
+    }
+
+    @GetMapping("/registerEmployee")
+    public String registerEmployee(){
+        return "registerEmployee";
+    }
+
+    @PostMapping("/save")
+    public Employee save(@ModelAttribute Employee employee, Model model){
+        service.save(employee);
+        Long id = service.save(employee).getId();
+        String message = "Record with id : '"+id+"' is saved successfully !";
+        model.addAttribute("message", message);
+        return "registerInvoicePage";
     }
 }
